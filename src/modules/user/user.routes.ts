0fd593e7767d862a2decfaaccb003603c456134c
abhishek701428 <abhishek@hitech.ai>
2024-09-21 14:07:24 +0530

    all code

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { registerUser, verifyOTP, loginAdmin } from '../user/user.controller';
+import { registerUser, verifyOTP, loginAdmin, loginUser } from '../user/user.controller';
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/verify-otp', verifyOTP);
 router.post('/login', loginAdmin);
+router.post('/login/user', loginUser);
 
 export default router;
